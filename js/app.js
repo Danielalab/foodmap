b951c71b3dbc5de4cc92e7ede85e62fa6c88ad6f
Danielalab <olabolacrayola@hotmail.com>
2017-12-29 01:25:12 -0500

    añade estructura y vista splash

---
+++ js/app.js
@@ -0,0 +1,11 @@
+$(document).ready(splahPage);
+
+// vista SPLASH
+function splahPage() {
+  setTimeout(function () {
+    $('.fly-in-text').removeClass('hidden');
+  }, 500);
+  setTimeout(function () {
+    window.location.href = 'views/view-1.html';
+  }, 5000);
+};
---
+++ js/data.js
@@ -0,0 +1,333 @@
+var restaurants = 
+{
+  ITA : 
+  [
+    {
+      name : m,
+      place : f,
+      money : [20,60],
+      delivery : true ,
+    },
+    {
+      name : m,
+      place : f,
+      money : [20,60],
+      delivery : true ,
+    },
+    {
+      name : m,
+      place : f,
+      money : [20,60],
+      delivery : true ,
+    },
+    {
+      name : m,
+      place : f,
+      money : [20,60],
+      delivery : true ,
+    },
+    {
+      name : m,
+      place : f,
+      money : [20,60],
+      delivery : true ,
+    },
+  ],
+  MEX : 
+  [
+    {
+      name : m,
+      place : f,
+      money : [20,60],
+      delivery : true ,
+    },
+    {
+      name : m,
+      place : f,
+      money : [20,60],
+      delivery : true ,
+    },
+    {
+      name : m,
+      place : f,
+      money : [20,60],
+      delivery : true ,
+    },
+    {
+      name : m,
+      place : f,
+      money : [20,60],
+      delivery : true ,
+    },
+    {
+      name : m,
+      place : f,
+      money : [20,60],
+      delivery : true ,
+    },
+  ] ,
+  CHI : 
+  [
+    {
+      name : m,
+      place : f,
+      money : [20,60],
+      delivery : true ,
+    },
+    {
+      name : m,
+      place : f,
+      money : [20,60],
+      delivery : true ,
+    },
+    {
+      name : m,
+      place : f,
+      money : [20,60],
+      delivery : true ,
+    },
+    {
+      name : m,
+      place : f,
+      money : [20,60],
+      delivery : true ,
+    },
+    {
+      name : m,
+      place : f,
+      money : [20,60],
+      delivery : true ,
+    },
+  ],
+  VEGA : 
+  [
+    {
+      name : m,
+      place : f,
+      money : [20,60],
+      delivery : true ,
+    },
+    {
+      name : m,
+      place : f,
+      money : [20,60],
+      delivery : true ,
+    },
+    {
+      name : m,
+      place : f,
+      money : [20,60],
+      delivery : true ,
+    },
+    {
+      name : m,
+      place : f,
+      money : [20,60],
+      delivery : true ,
+    },
+    {
+      name : m,
+      place : f,
+      money : [20,60],
+      delivery : true ,
+    },
+  ],
+  VEGE : 
+  [
+    {
+      name : m,
+      place : f,
+      money : [20,60],
+      delivery : true ,
+    },
+    {
+      name : m,
+      place : f,
+      money : [20,60],
+      delivery : true ,
+    },
+    {
+      name : m,
+      place : f,
+      money : [20,60],
+      delivery : true ,
+    },
+    {
+      name : m,
+      place : f,
+      money : [20,60],
+      delivery : true ,
+    },
+    {
+      name : m,
+      place : f,
+      money : [20,60],
+      delivery : true ,
+    },
+  ],
+  PER : 
+  [
+    {
+      name : m,
+      place : f,
+      money : [20,60],
+      delivery : true ,
+    },
+    {
+      name : m,
+      place : f,
+      money : [20,60],
+      delivery : true ,
+    },
+    {
+      name : m,
+      place : f,
+      money : [20,60],
+      delivery : true ,
+    },
+    {
+      name : m,
+      place : f,
+      money : [20,60],
+      delivery : true ,
+    },
+    {
+      name : m,
+      place : f,
+      money : [20,60],
+      delivery : true ,
+    },
+  ] ,
+  JAP : 
+  [
+    {
+      name : m,
+      place : f,
+      money : [20,60],
+      delivery : true ,
+    },
+    {
+      name : m,
+      place : f,
+      money : [20,60],
+      delivery : true ,
+    },
+    {
+      name : m,
+      place : f,
+      money : [20,60],
+      delivery : true ,
+    },
+    {
+      name : m,
+      place : f,
+      money : [20,60],
+      delivery : true ,
+    },
+    {
+      name : m,
+      place : f,
+      money : [20,60],
+      delivery : true ,
+    },
+  ] ,
+  KOR : 
+  [
+    {
+      name : m,
+      place : f,
+      money : [20,60],
+      delivery : true ,
+    },
+    {
+      name : m,
+      place : f,
+      money : [20,60],
+      delivery : true ,
+    },
+    {
+      name : m,
+      place : f,
+      money : [20,60],
+      delivery : true ,
+    },
+    {
+      name : m,
+      place : f,
+      money : [20,60],
+      delivery : true ,
+    },
+    {
+      name : m,
+      place : f,
+      money : [20,60],
+      delivery : true ,
+    },
+  ],
+  FAST : 
+  [
+    {
+      name : m,
+      place : f,
+      money : [20,60],
+      delivery : true ,
+    },
+    {
+      name : m,
+      place : f,
+      money : [20,60],
+      delivery : true ,
+    },
+    {
+      name : m,
+      place : f,
+      money : [20,60],
+      delivery : true ,
+    },
+    {
+      name : m,
+      place : f,
+      money : [20,60],
+      delivery : true ,
+    },
+    {
+      name : m,
+      place : f,
+      money : [20,60],
+      delivery : true ,
+    },
+  ],
+  POST : 
+  [
+    {
+      name : m,
+      place : f,
+      money : [20,60],
+      delivery : true ,
+    },
+    {
+      name : m,
+      place : f,
+      money : [20,60],
+      delivery : true ,
+    },
+    {
+      name : m,
+      place : f,
+      money : [20,60],
+      delivery : true ,
+    },
+    {
+      name : m,
+      place : f,
+      money : [20,60],
+      delivery : true ,
+    },
+    {
+      name : m,
+      place : f,
+      money : [20,60],
+      delivery : true ,
+    },
+  ] ,
+}
